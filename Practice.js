32115479908f486b4c886f33e7622a9db7b000bd
Feliax111 <rmv941@outlook.com>
2023-12-15 16:59:01 -0300

    Mi primer commit en una app

---
+++ Practice.js
@@ -0,0 +1,8 @@
+
+
+//var asigna espacio para una variable automaticamente
+//let asigname espacio para una variable que solo se usara localmente
+//const asigna espacio para una constante
+
+
+ellipse(200,200,300,350);
